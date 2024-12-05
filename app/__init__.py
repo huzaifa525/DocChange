2f13dc7393c1e1964f38035d05d03484c5e04d51
Huzefa  Nalkheda wala <53322981+huzaifa525@users.noreply.github.com>
2024-12-05 20:17:59 +0530

    Add app initialization files

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_cors import CORS
+from config import Config
+from app.middleware.auth import jwt_required
+from app.routes import api_bp
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    CORS(app)
+    app.config.from_object(config_class)
+    app.register_blueprint(api_bp, url_prefix='/api/v1')
+    return app
